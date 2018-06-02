16decfab5faf0aec46443088048e16e2899f2ee4
shtnkgm <nakagami.shota@gmail.com>
2018-06-03 01:01:13 +0900

    fix

---
+++ MyFramework/MyFramework/MyFramework.swift
@@ -0,0 +1,16 @@
+//
+//  MyFramework.swift
+//  MyFramework
+//
+//  Created by 中神 将太 on 2018/06/03.
+//  Copyright © 2018年 Shota Nakagami. All rights reserved.
+//
+
+import Foundation
+import SVProgressHUD
+
+class Foo {
+    init() {
+        SVProgressHUD.show()
+    }
+}
